cbea184e8c66a0e51692480372b976160d8f214e
AideenStrand <aideen.strandsson@gmail.com>
2021-01-12 18:27:17 +0100

    added name in list

--- src/Availabel.js
+++ src/Availabel.js
@@ -5,7 +5,7 @@ class Availabel extends React.Component {
     constructor() {
         super ();
         this.state = {
-            availabelData: "",
+            availabelData: {},
             count: 0
         }
     }
@@ -14,14 +14,19 @@ class Availabel extends React.Component {
         var response = []
         var i;
 
+
         console.log (this.state.count)
         fetch ("https://petstore.swagger.io/v2/pet/findByStatus?status=pending")
             .then (response => response.json ())
-            //.then(rep => console.log(rep))
-            .then (data => {
-                for (i = 0; i < data.length; i++) {
-                    response.push(data[i].id)
+           // .then(repo => console.log(repo))
+            .then(data => {
+                for(var i in data){
+                    response.push({
+                        id: data[i].id,
+                        name: data[i].name
+                    });
                 }
+                console.log(response)
             })
             .then (data => this.setState (prevState => {
                 return {
@@ -34,16 +39,36 @@ class Availabel extends React.Component {
     render() {
         return (
             <div className="leftPos">
-                <h3 className="h">Click for chosing id and put in your information <pre> or </pre> Click again and take
+                <h3 className="h">Click for chosing id and put in your information<pre>or</pre>Click again and take
                     new one:
                 </h3>
                 <button type="button" className="button" onClick={this.toggleButtonState}>Click</button>
-                <p>{this.state.availabelData}</p>
-
+                <p>{this.state.availabelData.id}</p>
+                <p>{this.state.availabelData.name}</p>
             </div>
 
         )
     }
 }
+
+/*
+var response = []
+var i;
+fetch ("https://petstore.swagger.io/v2/pet/findByStatus?status=available")
+    .then (response => response.json ())
+    .then (data => {
+        for (i = 0; i < data.length; i++) {
+            response["id"] = data[i].id
+            response["name"] = data[i].name
+            console.log (response)
+        }
+    })
+    .then (result =>
+        this.setState (
+            {
+                availabelData: response
+            }))
+}*/
+
 
 export default Availabel
