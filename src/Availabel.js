d136b7e28e48b0ea84ab3c3a950dc6661649b1d5
AideenStrand <aideen.strandsson@gmail.com>
2021-01-12 17:09:51 +0100

    added click

--- src/Availabel.js
+++ src/Availabel.js
@@ -5,56 +5,43 @@ class Availabel extends React.Component {
     constructor() {
         super ();
         this.state = {
-            availabelData: ""}
+            availabelData: "",
+          count:0}
     }
 
-    toggleButtonState = () => {
+    toggleButtonState = (event) => {
         var response = []
         var i;
+
+        console.log(this.state.count)
         fetch ("https://petstore.swagger.io/v2/pet/findByStatus?status=available")
             .then (response => response.json ())
             .then (data => {
                 for (i = 0; i < data.length; i++) {
-                    response["id"] = data[i].id
+                    response["id"] = data[this.state.count].id
                 }
+                console.log ("this is response")
                 console.log (response)
             })
-            .then (data => this.setState (
-                {availabelData: response["id"]}))
+            .then (data => this.setState( prevState => {
+                return {
+                    availabelData: response["id"],
+                    count: prevState.count  + 1
+                }}))
     }
 
     render() {
         return (
             <div className="leftPos" >
                 <h3  className="h">Choose id and put in your information please: </h3>
-                <button className="button" onClick={this.toggleButtonState}>Click</button>
+                <button type="button" className="button" onClick={this.toggleButtonState}>Click</button>
                 <p>{this.state.availabelData}</p>
+                <button type="button" className="button" onClick={this.toggleButtonState}>{this.state.count}</button>
 
             </div>
 
         )
     }
 }
-
-/*
-var response = []
-var i;
-fetch ("https://petstore.swagger.io/v2/pet/findByStatus?status=available")
-    .then (response => response.json ())
-    .then (data => {
-        for (i = 0; i < data.length; i++) {
-            response["id"] = data[i].id
-            response["name"] = data[i].name
-            console.log (response)
-        }
-    })
-    .then (result =>
-        this.setState (
-            {
-                availabelData: response
-            }))
-
-}*/
-
 
 export default Availabel
