7168420bbdcf9b75d698f9fc2bc9b2939113145f
AideenStrand <aideen.strandsson@gmail.com>
2021-01-12 17:36:22 +0100

    added click

--- src/Availabel.js
+++ src/Availabel.js
@@ -6,37 +6,39 @@ class Availabel extends React.Component {
         super ();
         this.state = {
             availabelData: "",
-          count:0}
+            count: 0
+        }
     }
 
     toggleButtonState = (event) => {
         var response = []
         var i;
 
-        console.log(this.state.count)
-        fetch ("https://petstore.swagger.io/v2/pet/findByStatus?status=available")
+        console.log (this.state.count)
+        fetch ("https://petstore.swagger.io/v2/pet/findByStatus?status=pending")
             .then (response => response.json ())
+            //.then(rep => console.log(rep))
             .then (data => {
                 for (i = 0; i < data.length; i++) {
-                    response["id"] = data[this.state.count].id
+                    response.push(data[i].id)
                 }
-                console.log ("this is response")
-                console.log (response)
             })
-            .then (data => this.setState( prevState => {
+            .then (data => this.setState (prevState => {
                 return {
-                    availabelData: response["id"],
-                    count: prevState.count  + 1
-                }}))
+                    availabelData: response[this.state.count],
+                    count: prevState.count + 1
+                }
+            }))
     }
 
     render() {
         return (
-            <div className="leftPos" >
-                <h3  className="h">Choose id and put in your information please: </h3>
+            <div className="leftPos">
+                <h3 className="h">Click for chosing id and put in your information <pre> or </pre> Click again and take
+                    new one:
+                </h3>
                 <button type="button" className="button" onClick={this.toggleButtonState}>Click</button>
                 <p>{this.state.availabelData}</p>
-                <button type="button" className="button" onClick={this.toggleButtonState}>{this.state.count}</button>
 
             </div>
 
